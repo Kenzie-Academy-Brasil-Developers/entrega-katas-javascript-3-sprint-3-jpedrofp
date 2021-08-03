ac109c0f3430de9c08937bd9797c64e506de7db5
João Pedro Ferreira Pillon <ferreirapillon.joaop@gmail.com>
2021-08-03 18:17:54 -0300

    Done

--- katas3.js
+++ katas3.js
@@ -1,76 +1,225 @@
-const sampleArray = [469, 755, 244, 245, 758, 450, 302, 20, 712, 71, 456, 21, 398, 339, 882, 848, 179, 535, 940, 472];
+function showResults(res) {
+    const katas = document.getElementById('katas3');
+    const conteudo = document.createElement('p');
+    conteudo.innerText = res;
+    conteudo.style.background= 'rgba(197, 239, 255, 0.562)';
+    katas.appendChild(conteudo);
+
+}
+const katas = document.getElementById('katas3');
+const titulo = document.createElement('h1');
+titulo.innerText = 'JS - KATAS 3';
+katas.appendChild(titulo);
+
+// function subTitle(tit) {
+// const k1 = document.getElementById("k1");
+// const p = document.createElement('p');
+// p.innerText = tit
+// k1.appendChild(p)
+// }
 
 function kata1() {
-    // implemente o código do kata 1 aqui
+    
+    array = [];
+    for (i = 1 ; i <= 25; i++) {
+     array.push(" "+i);
+    }
+    
+showResults(array);
+return array;
 }
+kata1();
+
 
 function kata2() {
-    // implemente o código do kata 2 aqui
+    array = [];
+    for (i = 25 ; i >= 1; i--) {
+    array.push(" "+i);
+    }
+    showResults(array);
+    return array;
 }
+(kata2())
 
 function kata3() {
-    // implemente o código do kata 3 aqui
+    array = [];
+    for (i = -1 ; i >= -25; i--) {
+    array.push(" "+i);
+    }
+    showResults(array);
+    return array;
 }
+(kata3())
 
 function kata4() {
-    // implemente o código do kata 4 aqui
+    array = [];
+    for (i = -25 ; i <= -1; i++) {
+    array.push(" "+i);
+    }
+    showResults(array);
+    return array;
 }
+(kata4())
+
+
 
 function kata5() {
-    // implemente o código do kata 5 aqui
+    array = [];
+    for (i = 25 ; i >= -25; i=i-2) {
+    array.push(" "+i);
+    }
+    showResults(array);
+    return array;
 }
+(kata5())
+
 
 function kata6() {
-    // implemente o código do kata 6 aqui
+    array = [];
+    for (i = 3 ; i <= 99; i=i+3) {
+    array.push(" "+i);
+    }
+    showResults(array);
+    return array;
 }
+(kata6())
 
 function kata7() {
-    // implemente o código do kata 7 aqui
+    array = [];
+    for (i = 7 ; i <= 98; i=i+7) {
+    array.push(" "+i);
+    }
+    showResults(array);
+    return array;
 }
+(kata7())
 
 function kata8() {
-    // implemente o código do kata 8 aqui
+    array = [];
+    for (i = 99 ; i >= 3; i=i-1) {
+    if (i % 7 === 0){
+        array.push(" "+i);
+    }
+    if (i % 3 === 0){
+        array.push(" "+i);
+    }
+    }
+    showResults(array);
+    return array;
 }
+(kata8())
 
 function kata9() {
-    // implemente o código do kata 9 aqui
+    array = [];
+    for (i = 5 ; i <= 100; i=i+5) {
+    if (i % 2 != 0) {
+        array.push(" "+i);
+    }
+    }
+    showResults(array);
+    return array;
 }
+(kata9())
+
+const sampleArray = [469, 755, 244, 245, 758, 450, 302, 20, 712, 71, 456, 21, 398, 339, 882, 848, 179, 535, 940, 472];
 
 function kata10() {
-    // implemente o código do kata 10 aqui
+    array = [];
+    for(i=0;i<=sampleArray.length-1;i++){
+        array.push(" "+sampleArray[i]);
+    }
+    showResults(array);
+    return sampleArray;
 }
+(kata10());
 
 function kata11() {
-    // implemente o código do kata 11 aqui
+    array = [];
+    for(i=0;i<=sampleArray.length-1;i++){
+        if (sampleArray[i] % 2 == 0) {
+        array.push(" "+sampleArray[i]);}
+    }
+    showResults(array);
+    return array;
 }
+(kata11());
+
 
 function kata12() {
-    // implemente o código do kata 12 aqui
+    array = [];
+    for(i=0;i<=sampleArray.length-1;i++){
+        if (sampleArray[i] % 2 != 0) {
+        array.push(" "+sampleArray[i]);}
+    }
+    showResults(array);
+    return array;
 }
+(kata12());
 
 function kata13() {
-    // implemente o código do kata 13 aqui
+    array = [];
+    for(i=0;i<=sampleArray.length-1;i++){
+        if (sampleArray[i] % 8 == 0) {
+        array.push(" "+sampleArray[i]);}
+    }
+    showResults(array);
+    return array;
 }
+(kata13());
 
 function kata14() {
-    // implemente o código do kata 14 aqui
+    array = [];
+    for(i=0;i<=sampleArray.length-1;i++){
+        array.push(" "+sampleArray[i]*sampleArray[i]);
+    }
+    showResults(array);
+    return array;
 }
+(kata14());
 
 function kata15() {
-    // implemente o código do kata 15 aqui
+    sum = 0;
+    for (i = 0 ; i <= 20; i++) {
+     sum+=i;
+    }
+showResults(sum);
+return sum;
 }
+kata15();
 
 function kata16() {
-    // implemente o código do kata 16 aqui
+    sum = 0;
+    for(i=0;i<=sampleArray.length-1;i++){
+    sum+=sampleArray[i];
+    }
+showResults(sum);
+return sum;
 }
+kata16();
 
 function kata17() {
-    // implemente o código do kata 17 aqui
+    let min = 999
+    for(i=0;i<=sampleArray.length-1;i++){
+    if (sampleArray[i] < min)
+    min = sampleArray[i];
+    }
+showResults(min);
+return min;
 }
+kata17();
+
 
 function kata18() {
-    // implemente o código do kata 18 aqui
+    let max = 0
+    for(i=0;i<=sampleArray.length-1;i++){
+    if (sampleArray[i] > max)
+    max = sampleArray[i];
+    }
+showResults(max);
+return max;
 }
+kata18();
+
 
 /**
  * Daqui em diante são os bônus, por sua conta e risco
